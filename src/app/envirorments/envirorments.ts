1cd05452e18ef5726a385edb8210d7e9dc9a7653
Enzo Samudio <enzosamudio@gmail.com>
2023-10-25 08:32:49 -0300

    modificacion git ingnore

--- src/app/envirorments/envirorments.ts
+++ src/app/envirorments/envirorments.ts
@@ -1,22 +1,22 @@
 export const environment = {
     production: true,
-    baseApiUrl: 'https://twhatsapp-api.onrender.com/',
+    baseApiUrl: '',
     firebase: {
-      apiKey: "AIzaSyAD2tdsGGE4bRPSCRvPIiAJdAeFx6uDPbo",
-      authDomain: "twhatsapp-a6696.firebaseapp.com",
-      databaseURL: "https://twhatsapp-a6696-default-rtdb.firebaseio.com",
-      projectId: "twhatsapp-a6696",
-      storageBucket: "twhatsapp-a6696.appspot.com",
-      messagingSenderId: "351317300295",
-      appId: "1:351317300295:web:531b18d03eec42d1bd4d62"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
     },
     firebaseConfigHosting: {
-      apiKey: "AIzaSyC6smDFUCs4v56Wt68BLwWbDoRn8j9eklw",
-      authDomain: "prana-whatsap-crm.firebaseapp.com",
-      projectId: "prana-whatsap-crm",
-      storageBucket: "prana-whatsap-crm.appspot.com",
-      messagingSenderId: "617526094680",
-      appId: "1:617526094680:web:90a755d7f534287fb9132d"
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
     }
     
   };
