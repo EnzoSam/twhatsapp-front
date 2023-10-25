289b9fe510483e6c6f9312f7c23aed9a8abcca99
Enzo Samudio <enzosamudio@gmail.com>
2023-10-25 08:42:58 -0300

    modificacion git ingnore

---
+++ src/app/envirorments/envirorments.expample.ts
@@ -0,0 +1,22 @@
+export const environment = {
+    production: true,
+    baseApiUrl: '',
+    firebase: {
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
+    },
+    firebaseConfigHosting: {
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
+    }
+    
+  };
