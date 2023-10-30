9442dc5cd878df582cd6f44471b7975ca7220ad1
Enzo Samudio <enzosamudio@gmail.com>
2023-10-30 09:25:04 -0300

    mejoras en colores

--- src/app/components/whatsapp/chat-item-list/chat-item-list.component.ts
+++ src/app/components/whatsapp/chat-item-list/chat-item-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 import { routesPaths } from 'src/app/constants/routes.contants';
+import { messageStatus } from 'src/app/constants/status.constats';
+import { IChange } from 'src/app/models/ichange.interface';
 import { IChat } from 'src/app/models/ichat.interface';
 import { ChatService } from 'src/app/services/chat.service';
 
@@ -27,4 +29,22 @@ export class MessageItemListComponent {
       scrollableDiv.scrollLeft = scrollableDiv.scrollWidth;
     }
   }
+
+  lastStatus(): string {
+
+    if(!this.chat || !this.chat.lastMessage || !this.chat.lastMessage.changes)
+      return '';
+
+    if(this.chat.lastMessage.changes.find((c: IChange)=>c.status === messageStatus.readed))
+      return  messageStatus.readed;
+    else if(this.chat.lastMessage.changes.find((c: IChange)=>c.status ===messageStatus.delivered))
+      return messageStatus.delivered;
+    else if(this.chat.lastMessage.changes.find((c: IChange)=>c.status === messageStatus.sended))
+      return messageStatus.sended;    
+    else if(this.chat.lastMessage.changes.find((c: IChange)=>c.status === messageStatus.failed))
+      return messageStatus.failed;    
+    else
+         return '';
+  }
+  
 }
